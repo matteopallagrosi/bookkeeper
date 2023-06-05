96a693fecad464b227fba915c1e8619809957b97
matteo <matteo.pallagrosi18@gmail.com>
2023-06-05 12:30:12 +0200

    Implemented jacoco coverage

---
+++ bookkeeper-benchmark/src/test/java/org/apache/bookkeeper/benchmark/FirstTest.java
@@ -0,0 +1,12 @@
+package org.apache.bookkeeper.benchmark;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void firsTest() {
+        Assert.assertTrue(true);
+    }
+}
