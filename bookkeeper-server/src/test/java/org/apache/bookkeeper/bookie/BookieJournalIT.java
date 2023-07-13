1e2f0a67d2c25ab8cc63b8a266592748bda71483
matteopallagrosi <matteo.pallagrosi18@gmail.com>
2023-07-13 16:33:17 +0200

    Modified integration test Bookie_Journal addLogEntry

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieJournalIT.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieJournalIT.java
@@ -6,52 +6,68 @@ import org.apache.bookkeeper.client.BKException;
 import org.apache.bookkeeper.conf.ServerConfiguration;
 import org.apache.bookkeeper.net.BookieId;
 import org.apache.bookkeeper.proto.BookkeeperInternalCallbacks.WriteCallback;
-import org.apache.bookkeeper.util.MathUtils;
 import org.apache.bookkeeper.util.PortManager;
 import org.apache.bookkeeper.bookie.storage.ldb.DbLedgerStorage;
 import org.apache.bookkeeper.common.allocator.PoolingPolicy;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import java.net.NetworkInterface;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.NetworkInterface;
+import java.io.File;
 import java.net.SocketException;
-import java.nio.ByteBuffer;
 import java.util.Collections;
 import java.util.Enumeration;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import static org.apache.bookkeeper.net.NetworkTopologyImpl.LOG;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
-public class provaIT {
+public class BookieJournalIT {
 
     @Rule
     public TemporaryFolder tempDir = new TemporaryFolder();
+    private ServerConfiguration serverConf;
+    private static final long LEDGER_ID = 1;
+    private static final long ENTRY_ID = 0;
+    private static final long LAST_ADD_CONFIRMED = -1;
+    private static final byte[] TEST_BYTES = "test".getBytes();
+
+    @Before
+    public void setUpEnvironment() {
+        try {
+            //crea delle directories temporanee (eliminate in automatico al termine del test) in cui i journal e i ledger saranno archiviati
+            File journalDir = tempDir.newFolder();
+            BookieImpl.checkDirectoryStructure(BookieImpl.getCurrentDirectory(journalDir));
+            File ledgerDir = tempDir.newFolder();
+            BookieImpl.checkDirectoryStructure(BookieImpl.getCurrentDirectory(ledgerDir));
+
+            //crea la configurazione del server necessaria a runnare il Bookie (gli aggancia le directories create)
+            this.serverConf = createServerConfiguration();
+            this.serverConf.setJournalDirName(journalDir.getPath())
+                    .setLedgerDirNames(new String[]{ledgerDir.getPath()})
+                    .setMetadataServiceUri(null);
+        }  catch (SocketException e) {
+            e.printStackTrace();
+            Assert.fail("Exception thrown during server configuration");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            Assert.fail("Exception thrown while creating temporary folder");
+        }
+    }
 
     @Test
     public void addLogEntryITCase() throws Exception {
-        //crea delle directories temporanee (eliminate al termine del test) in cui i journal e i ledger saranno archiviati
-        File journalDir = tempDir.newFolder();
-        BookieImpl.checkDirectoryStructure(BookieImpl.getCurrentDirectory(journalDir));
-        File ledgerDir = tempDir.newFolder();
-        BookieImpl.checkDirectoryStructure(BookieImpl.getCurrentDirectory(ledgerDir));
-
-        //crea la configurazione del server necessaria a runnare il Bookie (gli aggancia le directories create)
-        ServerConfiguration serverConf = createServerConfiguration();
-        serverConf.setJournalDirName(journalDir.getPath())
-                .setLedgerDirNames(new String[]{ledgerDir.getPath()})
-                .setMetadataServiceUri(null);
-
         TestBookieImpl b = spy(new TestBookieImpl(serverConf));
         b.start();
+
+        BookieId bookieId = BookieImpl.getBookieId(serverConf);
 
         //recupera il journal su cui l'operazione di addEntry dovrebbe essere registrata
         Journal expectedJournal = b.getJournal();
@@ -59,6 +75,16 @@ public class provaIT {
         //meccanismo ci sincronizzazione per attendere il completamento dell'operazione di addEntry
         CountDownLatch latch = new CountDownLatch(1);
 
+        //costruisce la entry da scrivere nel Bookie (rispettando la struttura indicata nella documentazione)
+        final ByteBuf entry = Unpooled.buffer();
+        entry.writeLong(LEDGER_ID);
+        entry.writeLong(ENTRY_ID);
+        entry.writeLong(LAST_ADD_CONFIRMED);
+        entry.writeBytes(TEST_BYTES);
+
+        String cxt = "test";
+
+        //costruisce la callback invocata al termine dell'operazione di scrittura sul journal della transazione di addEntry
         WriteCallback callBack = (rc, ledgerId, entryId, addr, ctx) -> {
             if (rc != BKException.Code.OK) {
                 LOG.error("Failed to write entry: {}", BKException.getMessage(rc));
@@ -66,9 +92,9 @@ public class provaIT {
                 return;
             }
 
-            //controlla che l'interazione tra il Bookie e il Journal sia avvenuta
+            //controlla che l'interazione tra il Bookie e il Journal sia avvenuta (con i parametri previsti)
             try {
-                verify(expectedJournal, atLeastOnce()).logAddEntry(any(ByteBuf.class), eq(false), any(WriteCallback.class), eq("foo"));
+                verify(expectedJournal, atLeastOnce()).logAddEntry(eq(entry), eq(false), any(WriteCallback.class), eq(cxt));
             } catch(InterruptedException e)  {
                 e.printStackTrace();
                 Assert.fail("Exception thrown while checking bookie-journal interaction");
@@ -77,16 +103,24 @@ public class provaIT {
             assertEquals(1, ledgerId);
             assertEquals(0, entryId);
 
+            //rilascia il "token" così da permettere al main thread di proseguire
             latch.countDown();
         };
 
         byte[] masterKey = new byte[64];
 
-        ByteBuf entry = buildEntry(1, 0, -1);
-
-        b.addEntry(entry, false, callBack , "foo",  masterKey);
+        b.addEntry(entry, false, callBack , cxt,  masterKey);
 
         latch.await();
+
+        //verifica se la entry è stata scritta correttamente (check di tutti i campi)
+        ByteBuf readEntry = b.readEntry(LEDGER_ID, ENTRY_ID);
+        assertEquals(readEntry.getLong(0), LEDGER_ID);
+        assertEquals(readEntry.getLong(8), ENTRY_ID);
+        assertEquals(readEntry.getLong(16), LAST_ADD_CONFIRMED);
+        byte[] data = new byte[TEST_BYTES.length];
+        readEntry.getBytes(24, data);
+        assertArrayEquals(TEST_BYTES, data);
 
         b.shutdown();
     }
@@ -95,7 +129,6 @@ public class provaIT {
         ServerConfiguration conf = new ServerConfiguration();
         conf.setTLSEnabledProtocols("TLSv1.2,TLSv1.1");
         conf.setJournalFlushWhenQueueEmpty(true);
-        // enable journal format version
         conf.setJournalFormatVersionToWrite(5);
         conf.setAllowEphemeralPorts(false);
         conf.setBookiePort(PortManager.nextFreePort());
@@ -120,17 +153,4 @@ public class provaIT {
 
         return conf;
     }
-
-    private WriteCallback getMockedCallBack() {
-        return mock(WriteCallback.class);
-    }
-
-    private static ByteBuf buildEntry(long ledgerId, long entryId, long lastAddConfirmed) {
-        final ByteBuf data = Unpooled.buffer();
-        data.writeLong(ledgerId);
-        data.writeLong(entryId);
-        data.writeLong(lastAddConfirmed);
-        return data;
-    }
-
 }
